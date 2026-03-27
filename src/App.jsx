6347bc9957ec0f6b1e426dab636db9bdd12bfb2d
muktabarzaki <jekkpramana@gmail.com>
2026-03-28 03:56:11 +0700

    terbaruu, udah nampil

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Dhika from "./pages/Dhika";
-import Hapiz from "./pages/Hapiz";
 import Zaki from "./pages/Zaki";
-import James from "./pages/James";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dhika-alghazali" element={<Dhika />} />
-        <Route path="/hapiz" element={<Hapiz />} />
         <Route path="/zaki" element={<Zaki />} />
-        <Route path="/james" element={<James />} />
       </Routes>
     </BrowserRouter>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Dhika from "./pages/Dhika";
-import Hapiz from "./pages/Hapiz";
-import Zaki from "./pages/Zaki";
-import James from "./pages/James";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Hanya panggil App.jsx
+import './index.css' // Untuk mengaktifkan Tailwind CSS 
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dhika-alghazali" element={<Dhika />} />
-        <Route path="/hapiz" element={<Hapiz />} />
-        <Route path="/zaki" element={<Zaki />} />
-        <Route path="/james" element={<James />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
