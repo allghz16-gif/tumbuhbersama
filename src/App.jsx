be4d0e51f2be19528a3ed5bfd0955b745f93a37e
muktabarzaki <jekkpramana@gmail.com>
2026-03-28 02:26:29 +0700

    nyoba lg

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import Dhika from "./Dhika.jsx";
 import Zaki from "./Zaki.jsx";
 import James from "./James.jsx";
 import Hapiz from "./Hapiz.jsx";
+import ain from "./main.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/dhika-alghazali" element={<Dhika />} />
-        <Route path="/rifqi-ardhian" element={<Rifqi />} />
-        <Route path="/ahmad-akbar" element={<Akbar />} />
+        <Route path="/rifqi-ardhian" element={<Zaki />} />
+        <Route path="/ahmad-akbar" element={<Hapiz />} />
+        <Route path="/rifqi-ardhian" element={<main />} />
+        <Route path="/ahmad-akbar" element={<James />} />
       </Routes>
     </BrowserRouter>
   );
