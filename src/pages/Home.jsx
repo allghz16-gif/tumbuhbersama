100b23af29ca67129218aadf658caafdb2ccdc7f
allghz16 <andiandika3608>
2026-03-28 01:41:14 +0700

    ubah

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Website Profil Kelompok</p>
+
+      <ul>
+        <li><a href="/dhika-alghazali">Dhika</a></li>
+        <li><a href="/zaki">Zaki</a></li>
+        <li><a href="/hapiz">Hapiz</a></li>
+        <li><a href="/james">James</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
